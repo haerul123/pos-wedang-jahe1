5441ff31635651dc8e632981fab7e0011922126e
aliahmadrojabialdiens <algiahle@gmail.com>
2026-07-11 17:42:16 +0700

    Fix: add base path for github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/pos-wedang-jahe1/', // <-- Tambahkan baris ini agar path asset benar di GitHub Pages
   server: {
     port: 3000,
     proxy: {
